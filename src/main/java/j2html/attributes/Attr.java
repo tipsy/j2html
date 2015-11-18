d15f0a5407ad796a4c2e26b640ce4bf9b537561a
thomas plorin <thomas@plorin.org>
2015-11-18 19:40:27 +0100

    added Font Tag

--- src/main/java/j2html/attributes/Attr.java
+++ src/main/java/j2html/attributes/Attr.java
@@ -110,5 +110,7 @@ public class Attr {
     public static String AUTOFOCUS = "autofocus";
     public static String AUTOPLAY = "autoplay";
     public static String AUTOSAVE = "autosave";
+    public static String FONT_FACE = "face";
+    
 
 }
