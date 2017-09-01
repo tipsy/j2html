4161be67bbe32471c00886b67cd9ede9c0fa0498
David <davidaase@hotmail.com>
2017-09-01 20:06:25 +0200

    Fix escaper-test

--- src/test/java/j2html/TextEscaperTest.java
+++ src/test/java/j2html/TextEscaperTest.java
@@ -1,31 +1,32 @@
 package j2html;
 
-import j2html.utils.SimpleEscaper;
-import j2html.utils.TextEscaper;
 import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
+import j2html.utils.SimpleEscaper;
+import j2html.utils.TextEscaper;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
 public class TextEscaperTest {
 
     @Test
-    public void testTextEscaper() throws Exception  {
+    public void testTextEscaper() throws Exception {
         assertThat("default text escaper is SimpleEscaper",
-                Config.textEscaper, is(instanceOf(SimpleEscaper.class)));
+                   Config.textEscaper, is(instanceOf(SimpleEscaper.class)));
 
         String expected = "&lt;div&gt;&lt;/div&gt;";
         assertThat("default text escaper works",
-                Config.textEscaper.escape("<div></div>"), is(expected));
+                   Config.textEscaper.escape("<div></div>"), is(expected));
 
         Config.textEscaper = new NoOpEscaper();
         assertThat("user can change text escaper implementation",
-                Config.textEscaper, is(instanceOf(NoOpEscaper.class)));
+                   Config.textEscaper, is(instanceOf(NoOpEscaper.class)));
 
         expected = "<div></div>";
         assertThat("user provided text escaper actually works",
-                Config.textEscaper.escape("<div></div>"), is(expected));
+                   Config.textEscaper.escape("<div></div>"), is(expected));
+        Config.textEscaper = new SimpleEscaper(); // reset escaper
     }
 
     private static class NoOpEscaper implements TextEscaper {
