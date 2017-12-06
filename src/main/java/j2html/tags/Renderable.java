7ebf4792b44262f5e0a7222c471b4fdbf56948be
David <davidaase@hotmail.com>
2017-12-06 14:08:24 +0100

    auto format

--- src/main/java/j2html/tags/Renderable.java
+++ src/main/java/j2html/tags/Renderable.java
@@ -26,11 +26,12 @@ public interface Renderable {
     default void render(Appendable writer) throws IOException {
         renderModel(writer, null);
     }
+
     /**
      * Render the Renderable and it's children using the supplied writer and a model.
      *
      * @param writer the current writer
-     * @param model a model object to provide data for children to render
+     * @param model  a model object to provide data for children to render
      */
     void renderModel(Appendable writer, Object model) throws IOException;
 }
