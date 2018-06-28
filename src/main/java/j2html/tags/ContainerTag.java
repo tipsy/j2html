b3940558776182473b73b6be13a61008206d5aee
Moandji Ezana <mwanji@gmail.com>
2018-06-29 01:53:04 +0200

    Use maven plugin to generate tag classes

--- src/main/java/j2html/tags/ContainerTag.java
+++ src/main/java/j2html/tags/ContainerTag.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class ContainerTag extends Tag<ContainerTag> {
+public class ContainerTag<T extends ContainerTag<T>> extends Tag<T> {
 
     private List<DomContent> children;
 
@@ -22,15 +22,15 @@ public class ContainerTag extends Tag<ContainerTag> {
      * @param child DomContent-object to be appended
      * @return itself for easy chaining
      */
-    public ContainerTag with(DomContent child) {
+    public T with(DomContent child) {
         if (this == child) {
             throw new RuntimeException("Cannot append a tag to itself.");
         }
         if (child == null) {
-            return this; // in some cases, like when using iff(), we ignore null children
+            return (T) this; // in some cases, like when using iff(), we ignore null children
         }
         children.add(child);
-        return this;
+        return (T) this;
     }
 
 
@@ -121,7 +121,7 @@ public class ContainerTag extends Tag<ContainerTag> {
      * @param text the text to be appended
      * @return itself for easy chaining
      */
-    public ContainerTag withText(String text) {
+    public T withText(String text) {
         return with(new Text(text));
     }
 
