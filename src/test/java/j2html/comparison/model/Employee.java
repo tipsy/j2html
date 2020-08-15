56a7cc0826a0995d9030c522fd15dcf284e6ab22
pointbazaar <alex23667@gmail.com>
2020-08-15 02:20:59 +0200

    fix Employee.java without lombok

--- src/test/java/j2html/comparison/model/Employee.java
+++ src/test/java/j2html/comparison/model/Employee.java
@@ -4,4 +4,22 @@ public class Employee {
     int id;
     String name;
     String title;
+
+    public Employee(int id, String name, String title) {
+        this.id = id;
+        this.name = name;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
