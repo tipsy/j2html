d334d4c8ae90ca24dac994f456c1b56c60f40e34
pointbazaar <alex23667@gmail.com>
2020-08-23 21:08:39 +0200

    fix it not building due to one attribute interface

--- src/main/java/j2html/tags/specialized/TrackTag.java
+++ src/main/java/j2html/tags/specialized/TrackTag.java
@@ -4,7 +4,7 @@ import j2html.tags.EmptyTag;
 import j2html.tags.attributes.*;
 
 public final class TrackTag extends EmptyTag<TrackTag>
-    implements IDefault<TrackTag>, IKind<TrackTag>, ILabel<TrackTag>, IOncuechange<TrackTag>, ISrc<TrackTag>, ISrclang<TrackTag> {
+    implements IDefault<TrackTag>, IKind<TrackTag>, ILabel<TrackTag>, ISrc<TrackTag>, ISrclang<TrackTag> {
     public TrackTag() {
         super("track");
     }
