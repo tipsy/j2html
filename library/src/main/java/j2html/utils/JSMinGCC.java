5bd30a87746b6cd6deed5c3f1a67bd8971ac19b8
reeelmay <nikita.1999.slipknot@gmail.com>
2021-11-15 00:36:31 +0300

    Impelmented Google Closure compiler integration for custom JS Minifier

---
+++ library/src/main/java/j2html/utils/JSMinGCC.java
@@ -0,0 +1,79 @@
+package j2html.utils;
+
+import com.google.javascript.jscomp.Compiler;
+import com.google.javascript.jscomp.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Class can be used to compress JS code using Google Closure Compiler.
+ * To learn more about GCC, please visit https://github.com/google/closure-compiler.
+ */
+public class JSMinGCC {
+    private CompilationLevel compilationLevel = CompilationLevel.SIMPLE_OPTIMIZATIONS;
+    private List<SourceFile> externs = new ArrayList<>();
+    private static final String INPUT_NAME = "input.js";
+
+    public JSMinGCC() {
+    }
+
+    public JSMinGCC(CompilationLevel compilationLevel, List<SourceFile> externs) {
+        this.compilationLevel = compilationLevel;
+        this.externs = externs;
+    }
+
+    /**
+     * Method can be used to change compilation level.
+     * Compilation level identifies witch algorithm will be used to compress js code.
+     *
+     * @param compilationLevelToSet compilation level to set
+     */
+    public void setCompilationLevel(CompilationLevel compilationLevelToSet) {
+        compilationLevel = compilationLevelToSet;
+    }
+
+    /**
+     * Method can be used to add external js files (externs).
+     * Externs are declarations that tell Closure Compiler the names of symbols
+     * that should not be renamed during advanced compilation.
+     *
+     * @param externsToSet externs to set
+     */
+    public void setExterns(List<SourceFile> externsToSet) {
+        externs = externsToSet;
+    }
+
+    /**
+     * Method can be used to get default browser environment variables.
+     * It is recommended to use browser externs when Advanced compilation level is set
+     */
+    public static List<SourceFile> getBrowserExterns() {
+        List<SourceFile> result = new ArrayList<>();
+        try {
+            result.addAll(CommandLineRunner.getBuiltinExterns(CompilerOptions.Environment.BROWSER));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * Method compresses js string using Google Closure Compiler with given settings.
+     *
+     * @param code the js-code you want to compress
+     * @return the compressed code
+     */
+    public String compressJS(String code) {
+        com.google.javascript.jscomp.Compiler compiler = new Compiler();
+        CompilerOptions options = new CompilerOptions();
+        options.setLanguageOut(CompilerOptions.LanguageMode.STABLE_OUT);
+        WarningLevel.QUIET.setOptionsForWarningLevel(options);
+        compilationLevel.setOptionsForCompilationLevel(options);
+        List<SourceFile> inputs = Collections.singletonList(SourceFile.fromCode(INPUT_NAME, code));
+        compiler.compile(externs, inputs, options);
+        return compiler.toSource();
+    }
+}
