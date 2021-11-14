0a365bb1cf70c45e47a8dcc95e59d56d2631bf38
reeelmay <nikita.1999.slipknot@gmail.com>
2021-11-15 00:41:20 +0300

    Import fixes

--- library/src/main/java/j2html/utils/JSMinGCC.java
+++ library/src/main/java/j2html/utils/JSMinGCC.java
@@ -1,7 +1,11 @@
 package j2html.utils;
 
+import com.google.javascript.jscomp.CompilationLevel;
+import com.google.javascript.jscomp.SourceFile;
+import com.google.javascript.jscomp.CommandLineRunner;
+import com.google.javascript.jscomp.CompilerOptions;
+import com.google.javascript.jscomp.WarningLevel;
 import com.google.javascript.jscomp.Compiler;
-import com.google.javascript.jscomp.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
